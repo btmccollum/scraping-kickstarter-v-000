a6e9bc0612d9bcc3229a3fd0707a239090dd667e
Brad McCollum <btmcco8@gmail.com>
2018-08-29 20:43:01 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,8 +7,6 @@ def create_project_hash
 
   projects = {}
 
-  #will need to iterate through the page we are parsing to create a hash with several keys such as imagelink,
-  #description, location, % funded, and amount funded
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
@@ -18,8 +16,6 @@ def create_project_hash
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i,
       :amount_funded => project.css("li.pledged span.money.usd.no-code").text.gsub(/[$,]/, "").to_i
     }
-    # binding.pry
+    binding.pry
   end
 end
-
-create_project_hash
