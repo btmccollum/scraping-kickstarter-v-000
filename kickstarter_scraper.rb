090557b5a9331514f4858cb1dbc9d3ace9e962fa
Brad McCollum <btmcco8@gmail.com>
2018-08-29 20:43:06 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,6 +16,6 @@ def create_project_hash
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i,
       :amount_funded => project.css("li.pledged span.money.usd.no-code").text.gsub(/[$,]/, "").to_i
     }
-    binding.pry
+    # binding.pry
   end
 end
